c8437d4a9f322f0e855bde5a8e7358f125766259
inventorbest <3364659916@qq.com>
2025-03-03 23:13:31 +0800

    feat(App): 添加计数器功能
    - 在 App 组件中引入 Jishuqi 组件
    - 实现了一个简单的计数器，点击按钮时计数增加
    - 添加了图片组件作为背景

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+
+var url = "https://www.dmoe.cc/random.php";
+function Picture() {
+  return (<img src={url} alt="img" title="img" style={{ width: "50%" }} />);
+}
+
+var Jishuqi = () => {
+  let [count, setCount] = useState(0);
+
+  return (<
+    div>
+      <Picture />
+      <button onClick={() => setCount(count + 1)} style={{color : "red"}}>{count}</button>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>My First React App</h1>
+    <div className="App">  
+      <Jishuqi />
     </div>
+  
   );
 }
 
